cb71531a158ebfc2fc4b877510f5095551e68106
Angel Villalobos <74730955+avillalobos5@users.noreply.github.com>
2021-08-17 16:29:57 -0700

    Update deck.js

--- War-Card-Game/deck.js
+++ War-Card-Game/deck.js
@@ -16,8 +16,21 @@ const VALUES = [
 ]
 
 export default class Deck {
-    constructor(cards = freshDeck()){
+    constructor(cards = freshDeck()) {
         this.cards = cards
+    }
+
+    get numberOfCards() {
+        return this.cards.length
+    }
+
+    shuffle() {
+        for (let i = this.numberOfCards - 1; i > 0; i--) {
+            const newIndex = Math.floor(Math.random() * (i+1))
+            const oldValue = this.cards[newIndex]
+            this.cards[newIndex] = this.cards[i]
+            this.cards[i] = oldValue
+        }
     }
 }
 

cb1152fdc9436c149b32fb2ad1f2486032b2179a
Angel Villalobos <74730955+avillalobos5@users.noreply.github.com>
2021-08-27 15:47:08 -0700

    Update script.js

--- War-Card-Game/script.js
+++ War-Card-Game/script.js
@@ -1,9 +1,10 @@
 import Deck from "./deck.js"
 
-const computerCardSlot = document.querySelector('.computer-card-slot')
+const computerCardSlot = document.querySelector(".computer-card-slot")
 
 const deck = new Deck()
 deck.shuffle()
 
 
 computerCardSlot.appendChild(deck.cards[0].getHTML())
+console.log(computerCardSlot)

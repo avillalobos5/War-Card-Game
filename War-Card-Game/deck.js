eb6761eb025f330c5ed098df7c71cda5c4cf5a0f
Angel Villalobos <74730955+avillalobos5@users.noreply.github.com>
2021-08-27 15:46:44 -0700

    Update deck.js

--- War-Card-Game/deck.js
+++ War-Card-Game/deck.js
@@ -39,7 +39,21 @@ class Card {
         this.suit = suit
         this.value = value
     }
+
+    get color() {
+        return this.suit === "♠" || this.suit === "♣" ? "black" : "red"
+    }
+
+    getHTML() { 
+        const cardDiv = document.createElement("div")
+        cardDiv.innerText = this.suit
+        cardDiv.classlist.add("card", this.color)
+        cardDiv.dataset.value = `${this.value} ${this.suit}`
+        return cardDiv
+    }
 }
+
+
 
 function freshDeck() {
     return SUITS.flatMap(suit => {
